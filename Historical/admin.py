7ef2257da4221f6c3fd618c8065edbb2f54a179d
lhcxnqm <604587316@qq.com>
2019-12-22 00:29:48 +0800

    add historySpider.py

--- Historical/admin.py
+++ Historical/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import History, BigOrSmall
+from .models import History, BigOrSmall, European
 
 # Register your models here.
 
 
 admin.site.register(History)
 admin.site.register(BigOrSmall)
+admin.site.register(European)
--- Historical/models.py
+++ Historical/models.py
@@ -4,29 +4,39 @@ from django.db import models
 
 
 class History(models.Model):
-    matchId = models.IntegerField(editable=False, unique=True)  # 赛事ID
+    matchId = models.CharField(max_length=8)  # 赛事ID
     match = models.CharField(max_length=10)  # 赛事
     round = models.CharField(max_length=10)   # 轮次
     time = models.DateField()                   # 比赛时间
     hostTeam = models.CharField(max_length=20)    # 主队
     guestTeam = models.CharField(max_length=20)   # 客队
-    result = models.CharField(max_length=10)    # 比赛结果
-    company = models.CharField(max_length=10)   # 公司
-    upperStage = models.FloatField()            # 上盘水位
-    lowerStage = models.FloatField()            # 下盘水位
-    opening = models.CharField(max_length=20)   # 开盘
-    win = models.FloatField()                   # 胜赔率
-    peace = models.FloatField()                 # 平赔率
-    lose = models.FloatField()                  # 负赔率
+    result = models.CharField(max_length=5)    # 比赛结果
+    companyId = models.CharField(max_length=3)   # 公司ID
+
+
+class Company(models.Model):
+    companyId = models.CharField(max_length=3)  # 公司ID
 
 
 class BigOrSmall(models.Model):
-    company = models.CharField(max_length=10)   # 公司
+    companyId = models.CharField(max_length=3)   # 公司ID
     immediateUpperStage = models.FloatField()      # 临场上盘水位
     immediateLowerStage = models.FloatField()      # 临场下盘水位
     immediateOpening = models.CharField(max_length=20)  # 临场大小球盘
     startUpperStage = models.FloatField()  # 初盘上盘水位
     startLowerStage = models.FloatField()  # 初盘下盘水位
     startOpening = models.CharField(max_length=20)  # 初盘大小球盘
+    # 表示外键关联到History表,当History表删除了该条数据,此表中也被删除
+    subMatchId = models.ForeignKey(History, to_field='matchId', on_delete=models.CASCADE)  # 赛事ID
+
+
+class European(models.Model):
+    companyId = models.CharField(max_length=3)  # 公司
+    immediateUpperStage = models.FloatField()  # 临场上盘水位
+    immediateLowerStage = models.FloatField()  # 临场下盘水位
+    immediateOpening = models.CharField(max_length=20)  # 临场盘口
+    startWin = models.FloatField()  # 初盘胜赔率
+    startPeace = models.FloatField()  # 初盘平赔率
+    startLose = models.FloatField()  # 初盘负赔率
     # 表示外键关联到History表,当History表删除了该条数据,此表中也被删除
     subMatchId = models.ForeignKey(History, to_field='matchId', on_delete=models.CASCADE)  # 赛事ID
